2ce34e16de0c6df7d99f3a653f4bf078a4ef3ede
이재민 <jmlee@commerce-league.com>
2022-11-21 14:04:57 +0900

    initial commit

---
+++ src/main/java/com/study/membershipwithtdd/MembershipWithTddApplication.java
@@ -0,0 +1,13 @@
+package com.study.membershipwithtdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MembershipWithTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MembershipWithTddApplication.class, args);
+	}
+
+}
